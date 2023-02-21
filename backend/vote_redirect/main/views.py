6f8631d4a430910fd15ac63d8849ee43a6ae69bc
liliyu <isliliyu@gmail.com>
2023-02-21 11:28:13 +0800

    add getdomain's add and update method

--- backend/vote_redirect/main/views.py
+++ backend/vote_redirect/main/views.py
@@ -21,3 +21,35 @@ class GetDomain(APIView):
         return JsonResponse(ret)
 
 
+    def post(self, request, *args, **kwargs):
+        ret = {'code': 200, 'msg': 'ok'}
+        try:
+            fun = json.loads(request.body).get('fun', None)
+            domainz = json.loads(request.body).get('domain', None)
+            status = json.loads(request.body).get('status', None)
+            if not domainz:
+                ret['code'] = 404
+                ret['msg'] = 'domain does not exist'
+                return JsonResponse(ret)
+            if fun == 'add':
+                if not status:
+                    ret['code'] = 400
+                    ret['msg'] = 'status error'
+                    return JsonResponse(ret)
+                domains = models.domain.objects.filter(domain=str(domainz))
+                if domains.exists():
+                    ret['code'] = 400
+                    ret['msg'] = 'domain already exist'
+                    return JsonResponse(ret)
+                domain_obj = models.domain.objects.create(domain=str(domainz), status=int(status))
+                domain_obj.save()
+            elif fun == 'update':
+                domain_obj = domain.objects.get(domain=str(domainz))
+                domain_obj.status = 0 if domain_obj.status else 1
+                domain_obj.save()
+            return JsonResponse(ret)
+        except Exception as e:
+            ret['code'] = 500
+            ret['msg'] = 'Timeout'
+            ret['error'] = str(e)
+        return JsonResponse(ret)
