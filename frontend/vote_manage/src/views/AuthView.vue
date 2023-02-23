5b473160adac6b1e0a89cc0d4877a20967c4c898
caro1xxx <caro1xxxhv@gmail.com>
2023-02-23 17:49:01 +0800

    auth page close btn

--- frontend/vote_manage/src/views/AuthView.vue
+++ frontend/vote_manage/src/views/AuthView.vue
@@ -2,7 +2,12 @@
   <div class="register"></div>
   <div class="register_mask">
     <div class="register_mask_body">
-      <img src="../assets/img/31.png" class="register_mask_close" alt="" />
+      <img
+        src="../assets/img/31.png"
+        @click="closeAuth"
+        class="register_mask_close"
+        alt=""
+      />
       <div class="register_mask_body_body">
         <div
           class="register_mask_body_login"
@@ -29,13 +34,22 @@
 import Register from "@/components/Register.vue";
 import Login from "@/components/Login.vue";
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 // 标志是否点击登录
 const authFlag = ref(true);
 
+const router = useRouter();
+
+// 改变注册或登录
 const changeAuth = (flag) => {
   if (flag === "login") authFlag.value = true;
   else authFlag.value = false;
+};
+
+// 关闭
+const closeAuth = () => {
+  router.push("/");
 };
 </script>
 
