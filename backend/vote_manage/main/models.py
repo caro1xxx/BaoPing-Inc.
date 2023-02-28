08611e156e3d161d91f33d03db7bb96ebb3b6574
liliyu <isliliyu@gmail.com>
2023-02-28 17:31:42 +0800

    update voteActivity structure

--- backend/vote_manage/main/models.py
+++ backend/vote_manage/main/models.py
@@ -51,7 +51,7 @@ class VoteActivity(models.Model):
     visit_count = models.IntegerField(default=0)
     visit_count_multiple = models.IntegerField(default=1)
     vote_count_restrict = models.TextField(default='')
-    today_start_voteuser = models.ForeignKey(VoteUser, to_field='open_id', on_delete=models.CASCADE, null=True)
+    today_start_voteuser = models.ForeignKey(VoteUser, to_field='open_id', on_delete=models.CASCADE, null=True, related_name='today_start_voteuser')
     today_star_update_begin_time = models.IntegerField(default=0)
     today_star_update_end_time = models.IntegerField(default=0)
     allowed_alone_everyday_vote_count = models.IntegerField(default=1)
@@ -61,6 +61,13 @@ class VoteActivity(models.Model):
     enable_vote_to_me = models.IntegerField(default=1)
     enable_comment = models.IntegerField(default=1)
     enable_vote_cert_code = models.IntegerField(default=1)
+    auto_comment_voteuser = models.ForeignKey(VoteUser, to_field='open_id', on_delete=models.CASCADE, null=True, related_name='auto_comment_voteuser')
+    auto_comment_begin_time = models.IntegerField(default=0)
+    auto_comment_end_time = models.IntegerField(default=0)
+    auto_comment_everyday_begin_time = models.IntegerField(default=0)
+    auto_comment_everyday_end_time = models.IntegerField(default=0)
+    auto_comment_space_minute = models.IntegerField(default=1)
+    auto_comment_everyday_count_strict = models.IntegerField(default=1)
 
 
 class Feedback(models.Model):
