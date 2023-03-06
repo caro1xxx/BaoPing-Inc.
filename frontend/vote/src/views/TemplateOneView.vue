420a93998842ac2a63ebcb0fdef92b321bebd975
yangjie <yangjie199902@outlook.com>
2023-03-06 08:59:50 +0800

    页面构建

--- frontend/vote/src/views/TemplateOneView.vue
+++ frontend/vote/src/views/TemplateOneView.vue
@@ -4,17 +4,8 @@
       <div class="content_top"></div>
       <div class="content_body">
         <div class="content_body_persennum">
-          <div class="content_body_persennum_item">入选企业：<span>458</span></div>
-          <div class="content_body_persennum_item">访问数量：<span>4999</span></div>
-        </div>
-        <div class="content_body_time">
-          <svg t="1677828286354" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2607" width="200" height="200">
-          <path d="M511.333 127.333c51.868 0 102.15 10.144 149.451 30.15 45.719 19.337 86.792 47.034 122.078 82.321 35.287 35.286 62.983 76.359 82.321 122.078 20.006 47.3 30.15 97.583 30.15 149.451s-10.144 102.15-30.15 149.451c-19.337 45.719-47.034 86.792-82.321 122.078-35.286 35.287-76.359 62.983-122.078 82.321-47.3 20.006-97.583 30.15-149.451 30.15s-102.15-10.144-149.451-30.15c-45.719-19.337-86.792-47.034-122.078-82.321-35.287-35.286-62.983-76.359-82.321-122.078-20.006-47.3-30.15-97.583-30.15-149.451s10.144-102.15 30.15-149.451c19.337-45.719 47.034-86.792 82.321-122.078 35.286-35.287 76.359-62.983 122.078-82.321 47.301-20.006 97.583-30.15 149.451-30.15m0-64c-247.424 0-448 200.576-448 448s200.576 448 448 448 448-200.576 448-448-200.576-448-448-448z" fill="#dbdbdb" p-id="2608">
-          </path><path d="M702.856 543.333H511.333c-17.673 0-32-14.326-32-32v-286.73c0-17.673 14.327-32 32-32s32 14.327 32 32v254.73h159.522c17.673 0 32 14.327 32 32s-14.326 32-31.999 32z" fill="#dbdbdb" p-id="2609"></path></svg>
-          <div class="content_body_time_label">倒计时结束</div>
-          <div class="content_body_time_num">
-            <span>00</span>天<span>00</span>时<span>00</span>分<span>00</span>秒
-          </div>
+          <div class="content_body_persennum_item">访问数：<span>2.5w</span></div>
+          <div class="content_body_persennum_item">报名数：<span>45</span></div>
         </div>
         <div class="content_body_search">
           <input type="text" placeholder="请输入选手名称或编号查询" />
@@ -63,7 +54,14 @@
         </div>
       </div>
     </div>
-    <div class="footer"></div>
+    <div class="footer">
+      <div>
+
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   </div>
 </template>
 
@@ -74,8 +72,6 @@
   width: 100%;
   height: 100%;
   display: flex;
-  flex-direction: column;
-  background-color: #f3f3f3;
 }
 .content{
   flex: 1;
@@ -84,9 +80,6 @@
 .content_top{
   height: 200px;
   background-color: red;
-}
-.content_body{
-  padding: 20px;
 }
 .content_body_persennum{
   display: flex;
@@ -98,50 +91,17 @@
   width: 41%;
   padding: 15px;
   font-size: 20px;
-  background-color: #ffffff;
   span{
-    color: rgb(61, 129, 61);
+    color: rgb(49, 60, 161);
+    font-weight: 50px;
   }
-}
-.content_body_time{
-  width: 94%;
-  padding: 10px 10px;
-  margin-top: 10px;
-  background-image: linear-gradient(to right, red , orange);
-  display: flex;
-  align-items: center;
-}
-.content_body_time_num{
-  margin-left: 10px;
-  flex: 1;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: #ffffff;
-  font-size: 12px;
-  span{
-    background-color: yellow;
-    padding: 5px 10px;
-    border-radius: 10px;
-    color: #000000;
-  }
-}
-svg{
-  width: 20px;
-  height: 20px;
-  margin-right: 10px;
-}
-.content_body_time_label{
-  font-size: 12px;
-  border: 2px solid #f3f3f3;
-  border-radius: 5px;
-  color: #f3f3f3;
-  padding: 2px 3px;
 }
 .content_body_search{
   display: flex;
   margin-top: 10px;
   justify-content: space-between;
+  background-color: #f3f3f3;
+  padding: 10px;
 }
 input{
   width: 70%;
