81b28b8388444f2b2e22de4f2cf8be1025f8b2ef
caro1xxx <caro1xxxhv@gmail.com>
2023-03-06 10:40:46 +0800

    fix

--- frontend/vote/src/views/TemplateTwoView.vue
+++ frontend/vote/src/views/TemplateTwoView.vue
@@ -1,16 +1,13 @@
 <template>
-    <div class="body">
-      1
-    </div>
-  </template>
-  
-  <script setup></script>
-  
-  <style lang="scss" scoped>
-  .body{
-    width: 100%;
-    height: 100%;
-    display: flex;
-  }
-  </style>
-  
+  <div class="body">1</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.body {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+</style>
