ab36e34046eab38e1db0fe2a48df4c5878c1a7ce
liliyu <isliliyu@gmail.com>
2023-03-07 16:58:15 +0800

    add gift delete method

--- backend/vote_manage/main/views/gift/Gift.py
+++ backend/vote_manage/main/views/gift/Gift.py
@@ -4,6 +4,7 @@ from django.core import serializers
 import json
 from main.views.Common import Common
 from main.views.gift.GiftOp import GiftOp
+from main import models
 
 
 class Gift(APIView):
@@ -51,6 +52,25 @@ class Gift(APIView):
                 return JsonResponse({'code': 400, 'msg': msg})
             print(1)
             giftOp.update(data)
+
+        except Exception as e:
+            ret = {'code': 500, 'msg': 'Timeout'}
+            # ret = {'code': 500, 'msg': 'Timeout', 'error': str(e)}
+        return JsonResponse(ret)
+    
+    def delete(self, request, *args, **kwargs):
+        ret = {'code': 200, 'msg': '删除成功'}
+        try:
+            pk = json.loads(request.body).get('pk', None)
+
+            if pk in [None, '']:
+                return JsonResponse({'code': 400, 'msg': '参数错误'})
+            giftOp = GiftOp()
+            ok, msg = giftOp.checkGiftIsExist(pk)
+            if not ok:
+                return JsonResponse({'code': 400, 'msg': msg})
+            
+            models.Gift.objects.filter(pk=pk).first().delete()
 
         except Exception as e:
             ret = {'code': 500, 'msg': 'Timeout'}
