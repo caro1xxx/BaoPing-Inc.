c5b4c6403da7937277d8dfbb77d09d720921f9ee
yangjie <yangjie199902@outlook.com>
2023-03-08 21:45:54 +0800

    二维码弹窗

---
+++ frontend/vote/src/components/isQrcode.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="body">
+        <div class="body_content">
+            <div>你已点赞成功</div>
+            <img
+              class='qscodeImg'
+              v-if="$store.state.settings[26].value"
+              :src="`${HOST}/media/${$store.state.settings[92].value}`"
+              alt=""
+            >
+        </div>
+        <img @click="returnPage" class="downImg" style="width: 30px;height: 30px;" src="../assets/images/39.png" alt="">
+    </div>
+</template>
+
+<script setup>
+import { defineEmits } from 'vue'
+import { useStore } from "vuex";
+import { HOST } from "../ENV";
+const emit = defineEmits(['returnPage'])
+const $store = new useStore()
+
+
+const returnPage = () => {
+    let params = {
+        status: false
+    }
+    emit('returnPage', params)
+}
+</script>
+
+<style lang="scss" scoped>
+.body{
+  position: absolute;
+  top: 0px;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background-color: #00000074;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 6;
+}
+.body_content{
+    width: 80%;
+    height: 40%;
+    background-color: #f3f3f3;
+    border-radius: 10px;
+}
+.body_content > div{
+    height: 40px;
+    font-size: 25px;
+    font-weight: 500;
+    text-align: center;
+    line-height: 40px;
+    margin-top: 20px;
+}
+.downImg{
+    position: absolute;
+    top: 245px;
+    left: 365px;
+}
+
+.qscodeImg{
+    width: 40%;
+    height: 40%;
+    display: block;
+    margin: 20px auto 0px;
+}
+</style>
